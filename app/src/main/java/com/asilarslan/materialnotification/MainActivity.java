4f976fc1e2e5c9145b876801e8fdaf688a709365
Asil Arslan <asilarslan93@gmail.com>
2015-06-29 16:16:06 +0300

    Initial commit

---
+++ app/src/main/java/com/asilarslan/materialnotification/MainActivity.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2015.
+ * Asil Arslan
+ * asilarslan93@gmail.com
+ */
+
+package com.asilarslan.materialnotification;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mNotificationTitles;
+
+    private static final Integer LIST_ITEM = 0;
+
+    public static final int[] COLOR = {0xFF666666, 0xFF96AA39,
+            0xFFC74B46, 0xFFF4842D, 0xFF3F9FE0, 0xFF5161BC};
+    public static final String[] LIST = {"Asil Arslan", "Asil Arslan",
+            "Asil Arslan", "Asil Arslan", "Asil Arslan", "Asil Arslan"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+        mNotificationTitles = mNotificationTitles = getResources().getStringArray(R.array.cities);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.mipmap.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mNotificationTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_notification) {
+            if (mDrawerLayout.isDrawerOpen(Gravity.RIGHT)) {
+                mDrawerLayout.closeDrawer(Gravity.RIGHT);
+            } else {
+                mDrawerLayout.openDrawer(Gravity.RIGHT);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new PostFragment();
+        Bundle args = new Bundle();
+        args.putInt(PostFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+
+    public static class PostFragment extends Fragment {
+        public static final String ARG_PLANET_NUMBER = "citiy_number";
+
+        public PostFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_notification, container, false);
+
+            ListView lv = (ListView) rootView.findViewById(R.id.listView1);
+            lv.setAdapter(new MyListAdapter(getActivity()));
+
+            return rootView;
+        }
+    }
+
+    private static class MyListAdapter extends BaseAdapter {
+        public MyListAdapter(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return LIST.length;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return true;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+
+            View item = convertView;
+            if (convertView == null) {
+                item = LayoutInflater.from(mContext).inflate(
+                        R.layout.post_list_item, parent, false);
+                item.setTag(LIST_ITEM);
+            }
+
+            TextView header = (TextView) item.findViewById(R.id.name);
+            header.setText(LIST[position % LIST.length]);
+
+            ImageView iv = (ImageView) item.findViewById(R.id.profile);
+            iv.setBackgroundColor(COLOR[position % COLOR.length]);
+
+            ImageView iv2 = (ImageView) item.findViewById(R.id.imageView);
+            iv2.setBackgroundColor(COLOR[position % COLOR.length]);
+
+
+            return item;
+        }
+
+
+        private final Context mContext;
+    }
+}
